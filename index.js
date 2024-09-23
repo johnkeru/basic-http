98572d285f5887e327bf23bea8c16ec7162b020f
john keru <keruk355@gmail.com>
2024-09-23 16:37:10 +0800

    crud with pages

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
 const express = require('express')
+const methodOverride = require('method-override')
 require('./dbConnect')()
 const Post = require('./Post')
+const path = require('path')
 const app = express()
 
 // parses json to object
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
+// app.set('views', path.join(__dirname, 'pages'))
 
 app.get('/posts', async (req, res) => {
     const posts = await Post.find()
-    res.status(200).json(posts)
+    // res.status(200).json(posts)
+    res.render('posts', { posts })
 })
 
-app.post('/posts', async (req, res) => {
+app.get('/post-form', (req, res) => res.render('create-post'))
+app.post('/post', async (req, res) => {
     try {
         const body = req.body
         const newPost = new Post(body)
         await newPost.save();
-        res.send('New post added successfully')
+        res.redirect('/posts')
     } catch {
         res.status(500).send('Server Error')
     }
 })
 // http://localhost:5000/posts/1312
+app.get('/post-form-update/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.render('update-post', { post })
+})
 app.put('/posts/:id', async (req, res) => {
     try {
         const id = req.params.id
         const body = req.body
         await Post.findByIdAndUpdate(id, body)
-        res.send('New post added successfully')
+        res.redirect('/posts')
     } catch (e) {
         res.status(500).send(e.message)
     }
 })
+
 app.delete('/posts/:id', async (req, res) => {
     const id = req.params.id
     await Post.findByIdAndDelete(id)
-    res.send('post deleted successfully')
+    res.redirect('/posts')
 })
 
 app.listen(5000, () => console.log('listening on: http://localhost:5000'));
