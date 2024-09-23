149ad1591009a6515ef4e38826e107482c2b5e15
john keru <keruk355@gmail.com>
2024-09-23 10:06:49 +0800

    second commit

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const http = require('node:http')
 const fs = require('fs')
 
 'http://localhost:5000/posts'
-const posts = [
-    { title: 'post 1', },
-    { title: 'post 2', }
+let posts = [
+    {
+        id: 1,
+        title: 'post 1'
+    },
+    {
+        id: 2,
+        title: 'post 2'
+    },
 ]
 
 const server = http.createServer((req, res) => {
@@ -14,8 +20,41 @@ const server = http.createServer((req, res) => {
             res.setHeader('Content-Type', 'application/json')
             res.end(JSON.stringify(posts))
         } else if (method === 'POST') {
-            res.setHeader('Content-Type', 'application/json')
-            res.end(JSON.stringify(posts))
+            let body = ''
+            req.on('data', chunk => body += chunk.toString())
+            req.on('end', () => {
+                const newPost = Object.assign(JSON.parse(body), { id: posts.length + 1 })
+                posts.push(newPost)
+                res.setHeader('Content-Type', 'application/json')
+                res.end(JSON.stringify(posts))
+            })
+        }
+        else if (method === 'PUT') {
+            let body = ''
+            req.on('data', chunk => body += chunk.toString())
+            req.on('end', () => {
+                const { id, title } = JSON.parse(body)
+                const post = JSON.parse(body)
+                console.log(post)
+                posts = posts.map(p => {
+                    if (p.id == post.id) {
+                        return { ...p, ...post }
+                    }
+                    return p
+                })
+                res.setHeader('Content-Type', 'application/json')
+                res.end(JSON.stringify(posts))
+            })
+        }
+        else {
+            let body = ''
+            req.on('data', chunk => body += chunk.toString())
+            req.on('end', () => {
+                const { id } = JSON.parse(body)
+                posts = posts.filter(p => p.id !== id)
+                res.setHeader('Content-Type', 'application/json')
+                res.end(JSON.stringify(posts))
+            })
         }
     } else {
         const pageContent = fs.readFileSync('404.html')
